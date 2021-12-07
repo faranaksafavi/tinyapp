d1dd6ff70fff6867010af85a2a4858027b11fb49
Faranak Safavi <faranak_safavi@yahoo.com>
2021-12-07 15:57:56 -0600

    adding delete button

--- express_server.js
+++ express_server.js
@@ -3,21 +3,20 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 function generateRandomString(len) {
-  let str="";
-  for (let i = 0; i < len; i++)
-  {
-    str += String.fromCharCode(Math.floor(Math.random() * 100) % 26 + 97)
-  };
+  let str = "";
+  for (let i = 0; i < len; i++) {
+    str += String.fromCharCode((Math.floor(Math.random() * 100) % 26) + 97);
+  }
   return str;
 }
 
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
@@ -32,7 +31,7 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  const templateVars = { greeting: 'Hello World!' };
+  const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
 });
 app.get("/urls", (req, res) => {
@@ -43,19 +42,33 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
   res.render("urls_show", templateVars);
 });
 app.post("/urls", (req, res) => {
   let short = generateRandomString(6);
   urlDatabase[short] = req.body["longURL"];
-  console.log(req.body);  // Log the POST request body to the console
- const templateVars = { shortURL: short, longURL: urlDatabase[req.params.shortURL] };
- res.render("urls_show", templateVars);
- res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body); // Log the POST request body to the console
+  const templateVars = {
+    shortURL: short,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 app.get("/u/:shortURL", (req, res) => {
-  console.log(`:::${req.params.shortURL}`)
+  console.log(`:::${req.params.shortURL}`);
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  console.log(req.body); // Log the POST request body to the console
+
+  res.redirect("/urls");
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
