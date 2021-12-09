35de5c2b1996d7b3d918e7c3c929154327cbf0f3
Faranak Safavi <faranak_safavi@yahoo.com>
2021-12-09 14:30:35 -0600

    removing bug for logging

--- express_server.js
+++ express_server.js
@@ -3,7 +3,25 @@ var cookieParser = require('cookie-parser')
 const app = express();
 app.use(cookieParser());
 const PORT = 8080; // default port 8080
+function lowercase(st) {
+  let result=""
+  st.split("").forEach((char) => { ((/[A-Z]/).test(char)) ? result += char.toLowerCase() : result += char; });
+  return result;
+};
+function searchDb(key, db) {
+  let result = null;
+  for (const it in db) {
+    console.log(`db[it]["username"]: ${db[it]["username"]}`)
+    if (db[it]["username"] == key) {
+      result = db[it];
 
+      break;
+    }
+  }
+  console.log(`search result: ${result}`)
+  return result;
+
+}
 function generateRandomString(len) {
   let str = "";
   for (let i = 0; i < len; i++) {
@@ -11,32 +29,25 @@ function generateRandomString(len) {
   }
   return str;
 }
-function searchDb(key, db) {
-  let result = null;
-  for (const it in db) {
-    if (db[key]["username"] == key) {
-      result = it;
-      break;
-    }
-  }
-  return result;
-
-}
-
-app.set("view engine", "ejs");
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-// dbs
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 const userDb = {
   b2xVn2: {
-    username: "fara",
+    a: "fara",
   password: "867"},
+  b: {
+    username: "pooyan",
+  password: "8234"},
 
 };
+
+
+app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+// dbs
 
 
 app.get("/", (req, res) => {
@@ -123,67 +134,76 @@ app.post("/urls/:shortURL/edit", (req, res) => {
   urlDatabase[req.params.shortURL]=req.body.edit;
   res.redirect(`/urls/${req.params.shortURL}`);
 });
-//redirect /urls
+
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
-  res.redirect("/urls");
+  console.log("i am in login state")
+  let email = req.body["email"];
+  let username  =lowercase(email);
+  let pass = req.body["password"];
+  let user = searchDb(username, userDb);
+  if (user) {
+   console.log(`user : ${Object.keys(user) }`)
+    console.log(`pass : ${pass}`);
+    let realPass = user["password"];
+    let logic = realPass === pass;
+    console.log(`logic: ${logic}`)
+    if (logic) {
+      req.cookies["username"] = username;
+      const templateVars = { urls: urlDatabase, authenticated: true, username: username };
+      console.log(`register username :${username}`);
+      res.render("urls_index",templateVars)
+    }
+    else{
+      let errors = "wrong username or password"
+      console.log(errors )
+      const templateVars = { urls: urlDatabase, authenticated:false, errors: errors};
+      res.render("login",templateVars)
+    }
+  }
+  else
+  {
+    let errors = "this email is not registered "
+    console.log(errors )
+    const templateVars = { urls: urlDatabase, authenticated:false, errors: errors};
+    res.render("login", templateVars);
+  }
+});
+
+app.get("/login", (req, res) => {
+
+  const templateVars = { urls: urlDatabase , authenticated:false, errors: ""};
+  res.status(403).render("login",templateVars)
+
+});
+app.get("/logout", (req, res) => {
+  res.clearCookie("username");
+  const templateVars = { urls: urlDatabase, authenticated : false };
+  res.status(200).render("urls_index",templateVars)
 });
 //redirect /urls
 app.post("/register", (req, res) => {
-  let username = req.body["email"].toLowerCase();
+  let email = req.body.email;
+  let username =lowercase(email) ;
   let password = req.body["password"];
-
-  if (!searchDb(username, userDb)) {
-
+  let user =searchDb(username, userDb)
+  console.log(`user :${user}`);
+  if (!user) {
     userDb[generateRandomString(8)] = { username, password };
     req.cookies["username"] = username;
-    const templateVars = { urls: urlDatabase, authenticated :true,username: username};
+    const templateVars = { urls: urlDatabase, authenticated: true, username: username };
+    console.log(`there is no ${username} ok to go`);
     res.render("urls_index",templateVars)
   } else {
     let errors = "this email already exist"
     const templateVars = { urls: urlDatabase, authenticated:false, errors: errors};
-    res.render("register",templateVars)
-  }
-});
-app.post("/login", (req, res) => {
-  let username = req.body["email"].toLowerCase();
-  let password = req.body["password"];
-
-  let user = searchDb(username, userDb);
-
-  if (!user) {
-    if (user[password] === password) {
-      req.cookies["username"] = username;
-      const templateVars = { urls: urlDatabase, authenticated :true,username: username};
-      res.render("urls_index",templateVars)
-    } else
-    {
-      let errors = "wrong username or password"
-      const templateVars = { urls: urlDatabase, authenticated:false, errors: errors};
-      res.render("register",templateVars)
-
-    }
-  } else {
-    let errors = "this email is not registered"
-    const templateVars = { urls: urlDatabase, authenticated:false, errors: errors};
-    res.render("register",templateVars)
+    console.log(`error :${errors}`);
+    res.render("register", templateVars)
   }
 });
 app.get("/register", (req, res) => {
+  let errors = "";
 
-  const templateVars = { urls: urlDatabase};
+  const templateVars = { urls: urlDatabase, authenticated :false, errors: errors };
   res.render("register",templateVars)
-
-});
-app.get("/login", (req, res) => {
-
-  const templateVars = { urls: urlDatabase};
-  res.render("login",templateVars)
-
-});
-app.post("/logout", (req, res) => {
-
-  const templateVars = { urls: urlDatabase};
-  res.render("urls_index",templateVars)
 
 });
